1b95b51f8a5cd0f32d4d02ba2de7ce4500d1bb2c
Eunyeop Kim <dmsduqzz@gmail.com>
2020-04-14 12:04:53 +0900

    json exception 응답 & form validation

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.douzone.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -11,28 +12,44 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.douzone.mysite.dto.JsonResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	private static final Log LOG = LogFactory.getLog(GlobalExceptionHandler.class);
-	
-	
+
 	@ExceptionHandler(Exception.class)
-	public void handlerException(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			Exception e) throws Exception {
+	public void handlerException(HttpServletRequest request, HttpServletResponse response, Exception e)
+			throws Exception {
 		// 1. 로깅(logging)
 		// e.printStackTrace();
 		StringWriter errors = new StringWriter(); // 버퍼
 		e.printStackTrace(new PrintWriter(errors));
 		LOG.error(errors.toString());
-				
-		// 2. 안내 페이지 가기(정상 종료)
-		request.setAttribute("exception", errors.toString());
-		request
-			.getRequestDispatcher("/WEB-INF/views/error/exception.jsp")
-			.forward(request, response);
+		// 2. 요청구분
+		// 만약, JSON 요청인 경우에는 request header의 ACcept에 application/json
+		// 만약, html 요청인 경우에는 request header의 ACcept에 text/html
+		// 만약, jpeg 요청인 경우에는 request header의 ACcept에 image/jpeg
+		String accept = request.getHeader("accept");
+		if (accept.matches(".*application/json.*")) {
+			// 3. JSON 응답
+			response.setStatus(HttpServletResponse.SC_OK);
+
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
+
+		} else {
+			// 2. 안내 페이지 가기(정상 종료)
+			request.setAttribute("exception", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/error/exception.jsp").forward(request, response);
+		}
+
 	}
 }
